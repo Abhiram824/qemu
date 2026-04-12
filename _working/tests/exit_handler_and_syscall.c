ccf51704f772cd7918ff887847fc2447b7068dce
Ojas Phirke <ojasphirke@utexas.edu>
2026-04-12 11:43:56 -0500

    complete syscall basic interposition. next need to port a native binary / library and add syscall checking

--- _working/tests/exit_handler_and_syscall.c
+++ _working/tests/exit_handler_and_syscall.c
@@ -17,7 +17,7 @@ asm(
     "exit_handler_scaffold:\n"
 
     // Save all registers to stack (we'll restore later)
-    "sub sp, sp, #256\n"  // Allocate space for 32 x64 regs
+    "sub sp, sp, #256\n" // Allocate space for 32 x64 regs
     "stp x0, x1, [sp, #0]\n"
     "stp x2, x3, [sp, #16]\n"
     "stp x4, x5, [sp, #32]\n"
@@ -41,12 +41,11 @@ asm(
     "bl exit_handler\n"
 
     // get PC from exit state (bits[63:2]), reconstruct full PC by shifting, and store temporarily
-    "" HFI_GES(0)  // Get exit state into x0
+    "" HFI_GES(0) // Get exit state into x0
 
     // Extract PC: shift right by 2 to get offset from bits[63:2], then shift left by 2
     "lsr x0, x0, #2\n"
     "lsl x0, x0, #2\n"
-    // "bl debugg\n"  // Debug: print the PC value
 
     // Store the target PC temporarily in x16 location on stack (will restore it as x16 value)
     "str x0, [sp, #128]\n"
@@ -60,7 +59,7 @@ asm(
     "ldp x10, x11, [sp, #80]\n"
     "ldp x12, x13, [sp, #96]\n"
     "ldp x14, x15, [sp, #112]\n"
-    "ldp x16, x17, [sp, #128]\n"  // x16 now has the target PC
+    "ldp x16, x17, [sp, #128]\n" // x16 now has the target PC
     "ldp x18, x19, [sp, #144]\n"
     "ldp x20, x21, [sp, #160]\n"
     "ldp x22, x23, [sp, #176]\n"
@@ -68,7 +67,7 @@ asm(
     "ldp x26, x27, [sp, #208]\n"
     "ldp x28, x29, [sp, #224]\n"
     "ldr x30, [sp, #240]\n"
-    "add sp, sp, #256\n"  // Deallocate stack space
+    "add sp, sp, #256\n" // Deallocate stack space
 
     // Jump back to the saved PC (x16 has the target PC from exit state)
     // add 4 to skip the instruction that caused the exit (the svc)'
@@ -76,22 +75,19 @@ asm(
     // load the options for the jump from the global variable (hfi_config)
     "adrp x17, hfi_config\n"
     "ldr x17, [x17, :lo12:hfi_config]\n"
-    
-    "mov x0, x16\n"  // Move target PC to x0 for HFI_ENTER
 
-
-    "" HFI_ENTER(17, 16)  // Re-enter HFI mode with options in x17, target in x16
+    "" HFI_ENTER(17, 16) // Re-enter HFI mode with options in x17, target in x16
 );
 
 void debugg(uint64_t x) {
-    char buffer[100];
-    snprintf(buffer, sizeof(buffer), "[DEBUGG] Exit handler scaffold: target PC = 0x%llx\n", (unsigned long long)x);
-    perror(buffer);
+    exit(x);
 }
 
-void mock_syscall(uint64_t regs[31]) {
+uint64_t do_syscall(uint64_t regs[31])
+{
     // copy regs x0-x5 and x8 and then make svc
     // note the additional clobbers x6-x7, x16-x17, x30, NZCV
+    uint64_t result;
     asm volatile(
         "mov x0, %[x0]\n"
         "mov x1, %[x1]\n"
@@ -101,12 +97,15 @@ void mock_syscall(uint64_t regs[31]) {
         "mov x5, %[x5]\n"
         "mov x8, %[x8]\n"
         "svc #0\n"
-        :
+        "str x0, %[result]\n"
+        : [result] "=m"(result)
         : [x0] "r"(regs[0]), [x1] "r"(regs[1]), [x2] "r"(regs[2]), [x3] "r"(regs[3]), [x4] "r"(regs[4]), [x5] "r"(regs[5]), [x8] "r"(regs[8])
         : "x6", "x7", "x16", "x17", "x30", "cc");
+    return result;
 }
 
-void exit_handler(uint64_t regs[31]) {
+void exit_handler(uint64_t regs[31])
+{
     // printf("[EXIT_HANDLER] Exit handler called!\n");
 
     // check if its a syscall
@@ -114,11 +113,14 @@ void exit_handler(uint64_t regs[31]) {
     uint64_t exit_reason = HFI_EXIT_STATE_GET_REASON(exit_state);
     uint64_t exit_pc = HFI_EXIT_STATE_GET_PC(exit_state);
 
-    if (exit_reason == HFI_SYSCALL_REQUESTED) {
+    if (exit_reason == HFI_SYSCALL_REQUESTED)
+    {
         // printf("[HARNESS] Syscall requested at PC: 0x%llx\n", (unsigned long long)exit_pc);
-        // printf("hi\n");
-        mock_syscall(regs);
-    } else if (exit_reason == HFI_EXIT_CALLED) {
+        uint64_t result = do_syscall(regs);
+        regs[0] = result;
+    }
+    else if (exit_reason == HFI_EXIT_CALLED)
+    {
         // printf("[HARNESS] HFI exit called at PC: 0x%llx\n", (unsigned long long)exit_pc);
         asm volatile(
             "mov sp, %[hfi_end_sp]\n"
@@ -129,7 +131,8 @@ void exit_handler(uint64_t regs[31]) {
     }
 }
 
-void code_under_hfi() {
+void code_under_hfi()
+{
     uint64_t x = 9;
     x += 3;
 
@@ -139,18 +142,19 @@ void code_under_hfi() {
 
     asm volatile(
         "mov x0, %0\n"
-        "" HFI_EXIT()  // Exit HFI mode, should jump to exit handler
+        "" HFI_EXIT() // Exit HFI mode, should jump to exit handler
         :
         : "r"(x)
         : "x0");
 }
 
-int test_hfi_enter_exit(void) {
+int test_hfi_enter_exit(void)
+{
     void (*f)(void) = code_under_hfi;
     hfi_config = HFI_OPT_LOCK_REGIONS;
 
     // get sandbox vars
-    uint64_t code_region_base = (uint64_t)(f) & (~0xFFFFull);  // page-align the base
+    uint64_t code_region_base = (uint64_t)(f) & (~0xFFFFull); // page-align the base
     uint64_t code_region_mask = ~0xFFFFull;
     uint64_t code_region_perms = HFI_PERM_EXEC;
     uint64_t exit_handler_addr = (uint64_t)exit_handler_scaffold;
@@ -165,18 +169,18 @@ int test_hfi_enter_exit(void) {
 
     // enter the sandbox
     asm volatile(
-        "mov x20, %[f]\n"           // Move function pointer to x20
-        "mov x21, %[hfi_config]\n"  // Move hfi_config to x21
+        "mov x20, %[f]\n"          // Move function pointer to x20
+        "mov x21, %[hfi_config]\n" // Move hfi_config to x21
 
         // save trusted context
         "adr x22, 1f\n"
         "str x22, %[hfi_end_addr]\n"
         "mov x22, sp\n"
         "str x22, %[hfi_end_sp]\n"
-        "" HFI_ENTER(21, 20)  // Enter HFI mode with region 1 locked (arbitrary choice for test)
+        "" HFI_ENTER(21, 20) // Enter HFI mode with region 1 locked (arbitrary choice for test)
         "1:\n"
-        : [hfi_end_addr] "=m"(hfi_end_addr), [hfi_end_sp] "=m"(hfi_end_sp)  // Output operand
-        : [f] "r"(f), [hfi_config] "r"(hfi_config)                          // Input operands
+        : [hfi_end_addr] "=m"(hfi_end_addr), [hfi_end_sp] "=m"(hfi_end_sp) // Output operand
+        : [f] "r"(f), [hfi_config] "r"(hfi_config)                         // Input operands
         : "x29", "x30");
 
     return 5;
--- target/arm/tcg/helper-hfi.c
+++ target/arm/tcg/helper-hfi.c
@@ -37,7 +37,7 @@ void HELPER(debuglog)(CPUARMState* env, uint32_t id) {
     static int x = 0;
     // uint64_t x8val = env->xregs[8];
     // uint64_t hfi_enabled = env->hfi.control_config.reg_enabled;
-    // qemu_log("\tq[QEMU_DEBUG_LOG] id=%-3u | x=%-3d | x8=%-7ld | hfienable=%-lx\n", id, x, x8val, hfi_enabled);
+    // qemu_log("\tqemu[QEMU_DEBUG_LOG] id=%-3u | x=%-3d | x8=%-7ld | hfienable=%-lx\n", id, x, x8val, hfi_enabled);
     x++;
 }
 
@@ -288,7 +288,7 @@ void HELPER(hfi_enter)(CPUARMState* env, uint64_t jump_target, uint64_t options)
     env->hfi.control_config.reg_config_opts = options;
     env->pc = jump_target;
 
-    qemu_log("\tq[HFI_ENTER] pc=%lx\n", env->pc);
+    qemu_log("\tqemu[HFI_ENTER] pc=%lx\n", env->pc);
 
     /* Exit to main loop so it restarts at the new PC */
     cpu_loop_exit(cpu);
@@ -305,7 +305,7 @@ void HELPER(hfi_enter)(CPUARMState* env, uint64_t jump_target, uint64_t options)
 void HELPER(hfi_exit)(CPUARMState* env, uint32_t exit_cause) {
     CPUState* cpu = env_cpu(env);
 
-    qemu_log("\tq[HELPER] Exiting HFI cause=%u | pc=%lx\n", exit_cause, env->pc);
+    qemu_log("\tqemu[HELPER] Exiting HFI cause=%u | pc=%lx\n", exit_cause, env->pc);
     
     /* Cannot exit if not currently in HFI mode */
     if (!hfi_is_enabled(env)) {
